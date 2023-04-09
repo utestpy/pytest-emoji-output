e6c93402d52f49ca192e3ab3d4464be8b6007959
Volodymyr Yahello <vyahello@gmail.com>
2023-04-09 10:52:56 +0300

    Release 0.2.1 plugin version

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,6 +1,6 @@
 """Package provides API for testing output with emoji."""
 
-__version__: str = "0.2.0"
+__version__: str = "0.2.1"
 __package_name__: str = "pytest-emoji-output"
 __author__: str = "Volodymyr Yahello"
 __email__: str = "vyahello@gmail.com"
